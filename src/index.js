9cf67bd18f6c8ccce59d5b39c16e7116e7cbcd57
Rikisan <rimpa007@hotmail.com>
2018-11-07 18:10:44 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const  express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path')
+
+const { mongoose } = require('./database');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks' , require('./routes/task.routes.js'));
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Staring the server
+
+app.listen(app.get('port'),() =>{
+    console.log(`Server on port ${app.get('port')}`);
+});
